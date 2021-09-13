956aa0ca902525720ea13f49a8bb6119699f6ceb
developerTimur <timurul65@gmail.com>
2021-09-13 13:28:10 +0400

    files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+include "vendor/autoload.php";
+
+use GuzzleHttp\Client;
+
+if(isset($_POST['url'])){
+    try{
+        $client = new Client(['timeout'  => 2.0,'allow_redirects' => false,'http_errors' => true]);
+        $response = $client->get($_POST['url']);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode([
+            "status" => $response->getStatusCode(),
+            "body" => $response->getBody()->getContents(),
+        ]);
+    }catch (Exception $e){
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode([
+            "status" => $e->getCode(),
+            "body" => [
+                $e->getMessage(),
+                $e->getCode()
+            ]
+        ]);
+    }
+}
+
